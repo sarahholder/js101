d7f8059f82920296fda735924a86a4cb0114fd4b
Sarah Holder <sr.holder@yahoo.com>
2020-01-20 19:10:31 -0600

    finished function challenge

--- functions.js
+++ functions.js
@@ -11,16 +11,34 @@ console.log(nuggetizer('chicken'));
 console.log(nuggetizer('pig'));
 console.log(nuggetizer('cow'));
 console.log(nuggetizer('fish'));
-<<<<<<< HEAD
-console.log(nuggetizer('tire'));
-=======
 console.log(nuggetizer('tire'));
 
-const dogBreed = () => {
+const dogBreed = (dog) => {
     return `my favorite dog breed is ${dog}`
-
 };
 
+
 console.log(dogBreed('lab'));
-console.log(dogBreed(''));
->>>>>>> 94c2430043e5d3e0fa1d3271b83cd556d559cfce
+console.log(dogBreed('lab'));
+
+//challenge 1
+// Write a function called fortyTwo that takes any 
+// number and adds 42
+const fortyTwo = (num) => {
+    return num + 42
+}
+
+console.log(fortyTwo(4)); //46
+
+
+//challenge 2
+// Write a function called oldAge that takes in the year
+// someone was born and figures out how old you will be in 2099
+const oldAge = (yearBorn) => {
+    return 2099 - yearBorn;
+}
+console.log(oldAge(1954));//145
+console.log(oldAge(2000));//
+
+
+
