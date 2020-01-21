423c05c1e8da5824b89d163745fc50295ae72a7d
Sarah Holder <sr.holder@yahoo.com>
2020-01-20 18:58:51 -0600

    added functions

---
+++ functions.js
@@ -0,0 +1,22 @@
+console.log("Functions")
+
+const nuggetizer = (animal) => {
+    const output = `processed ${animal}`;
+    //const output = 'processed ' + animal;
+    return output;
+
+}
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('tire'));
+
+const dogBreed = () => {
+    return `my favorite dog breed is ${dog}`
+
+};
+
+console.log(dogBreed('lab'));
+console.log(dogBreed(''));
