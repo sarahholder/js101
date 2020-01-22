74b41dfcbdf1ad4f5ca39ef1c7f61def78e6daf9
Sarah Holder <sr.holder@yahoo.com>
2020-01-21 18:47:56 -0600

    worked on combos need to finish

--- combo.js
+++ combo.js
@@ -3,23 +3,44 @@ console.log('COMBO');
 // Create a function called evenOdd that takes an integer as an argument and
 // returns "Even" for even numbers or "Odd" for odd numbers.
 
-const evenOdd = (num) => {
-    if (num % 2 === 0){
-        console.log('even number');
-    }else{
-        console.log('odd number');
-    };   
 
-}
-console.log(evenOdd(4));
-console.log(evenOdd(5));
+//const evenOdd = (num) => {
+ //   if (num % 2 === 0){
+ //       return even;
+
+ //   }else{
+ //       return odd;
+ //   };   
+
+//}
+console.log('evenodd', evenOdd(4));
+console.log('evenodd', evenOdd(5));
 
 // Challenge #2
 // given a number, find its opposite. 2 = -2
+//const opposite = (num) => {
+//    return* (-1);  
+//};
+//console.log('opposite', opposite(3));
+
 // Challenge #3
 // A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 // Console true if the given string is a palindrome. Otherwise, console false.
+const palindrome = (text) => {
+    const cleanText = text.toLowerCase().replace(' ', '');
+    const opp = text.split('').reverse().join('');
+    console.log('opp', opp);
+    console.log('text', opp);  
+return (cleanText === opp);
+};
+
+console.log('palindrone', palindrome('cat'));
+console.log('palindrone', palindrome('mom'));
+console.log('palindrone', palindrome('bear'));
+console.log('palindrone', palindrome('racecar'));
+
 // Challenge #4
 // Lawrence the wide mouth frog is particularly interested in the eating habits of other creatures.
 // He just can't stop asking the creatures he encounters what they like to eat. But then he meet the alligator who just LOVES to eat the lips of wide-mouthed frogs!
-// Given a string with the animal name, that Lawrence encounters, output small if the animal is an alligator (case insensitive) otherwise return wide.
+// Given a string with the animal name, that Lawrence encounters, output small if the animal is an alligator (case insensitive) otherwise return wide
+//
---
+++ combo2.js
@@ -0,0 +1,8 @@
+const animal = (str) => {
+    if(str.toLowerCase() === 'alligator'){
+        return 'small';
+    }else{
+        return 'wide';
+    }
+};
+console.log
